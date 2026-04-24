48b101b757859f26b61bf05f57954ba04aa18e5c
prince <princeaman635@gmail.com>
2026-04-25 00:56:27 +0200

    fix: rebuild dist avec cors production

--- apps/api/dist/apps/api/src/index.js
+++ apps/api/dist/apps/api/src/index.js
@@ -32,6 +32,7 @@ app.use((0, cors_1.default)({
     origin: [
         'http://localhost:3000',
         'http://localhost:3001',
+        'https://shopeasy-1-kahg.onrender.com',
     ],
     credentials: true,
 }));
