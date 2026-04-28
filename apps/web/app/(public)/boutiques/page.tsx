4720941b621017ad978380b2adee6b802ce6f657
prince <princeaman635@gmail.com>
2026-04-28 08:23:55 +0200

    fix: fermeture balise Link boutique annuaire

--- apps/web/app/(public)/boutiques/page.tsx
+++ apps/web/app/(public)/boutiques/page.tsx
@@ -216,7 +216,7 @@ export default function BoutiquesPage() {
                       Visiter la boutique
                     </span>
                   </div>
-                </>
+                </Link>
               ))}
             </div>
 
